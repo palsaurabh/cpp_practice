72fcd020bda9a15be4c4bcb14aa35a478479c5cf
saurabh <saurabh6321@gmail.com>
2025-05-07 15:56:38 +0000

    Non Working gtest setup with bazel

---
+++ practice/factory_example/factory_ex_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "factory_ex.hpp"
+
+TEST(FactoryExampleTest, TestCase1) {
+    // Add your test logic here
+    ASSERT_TRUE(true);
+}
+
+TEST(FactoryExampleTest, TestCase2) {
+    // Add your test logic here
+    ASSERT_EQ(1, 1);
+}
